0e72434c7de8bc2a0728ffbc6f91f22c951ffa7c
kayoooooo <k.yao.kayoo@gmail.com>
2022-12-22 10:28:15 -0500

    new colors

--- src/pieces.js
+++ src/pieces.js
@@ -1,4 +1,4 @@
-export const colors = ["cyan", "blue", "orange", "yellow", "green", "purple", "red"]
+export const colors = ["cyan", "blue", "orange", "yellow", "lime", "fuchsia", "red"]
 const shapes = [[[0,0,0,0], [1,1,1,1], [0,0,0,0],[0,0,0,0]], 
 [[2,0,0], [2,2,2], [0,0,0]], 
 [[0,0,3], [3,3,3], [0,0,0]], 
